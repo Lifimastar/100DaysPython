79d74d2cf82f05d6a4f63dea178d9a4b9ef8e26c
Luis Urdaneta <luisfernand2000@gmail.com>
2024-04-16 18:32:21 -0400

    day 13 & 14
    Create a High-lower game, I finished python beginer course, now it's time for intermediate level

---
+++ day13/exercise_31.py
@@ -0,0 +1,16 @@
+# Debug Odd or Even Exercise
+
+# number = int(input("Wich number do you want to check? "))
+
+# if number % 2 = 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+# Fixed
+number = int(input("Wich number do you want to check? "))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
